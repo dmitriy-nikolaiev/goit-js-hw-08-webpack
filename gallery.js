34438f68f33823f34994f3dbc9652fca0f555810
Dmitriy Nikolaiev <dimnik.u11@gmail.com>
2021-03-18 09:32:01 +0200

    Добавил исправление

--- gallery.js
+++ gallery.js
@@ -5,17 +5,15 @@ const lightboxRef = document.querySelector('div.js-lightbox');
 const lightboxImageRef = document.querySelector('img.lightbox__image');
 
 // Create gallery
-const htmlGalleryItems = galleryItems.map(
-  ({ preview, original, description: alt }) => {
-    return `<li class="gallery__item">
+const htmlGalleryItems = galleryItems.map(({ preview, original, description: alt }) => {
+  return `<li class="gallery__item">
     <a class="gallery__link" href="${original}">
       <img class="gallery__image" src="${preview}"
       data-source="${original}"
       alt="${alt}"/>
     </a>
   </li>`;
-  },
-);
+});
 galleryListRef.insertAdjacentHTML('afterbegin', htmlGalleryItems.join(''));
 
 const closeModal = () => {
@@ -25,7 +23,7 @@ const closeModal = () => {
 };
 
 // Lightbox scroll image
-const scrollImage = direction => {
+const scrollImage = (direction) => {
   // let nextImageIndex =
   //   galleryItems.findIndex(
   //     element => element.original === lightboxImageRef.src,
@@ -41,17 +39,14 @@ const scrollImage = direction => {
   // 8 + 9 + 1: 18 % 9 = 0  если последний и +, то 0
   // 0 + 9 - 1: 8 % 9 = 2   если первый и -,    то 8
   // 8 + 0 - 1: 7 % 9 = 7   если последний и -, то 7
-  let nextImageIndex = galleryItems.findIndex(
-    ({ original }) => original === lightboxImageRef.src,
-  );
-  nextImageIndex =
-    (nextImageIndex + galleryItems.length + direction) % galleryItems.length;
+  let nextImageIndex = galleryItems.findIndex(({ original }) => original === lightboxImageRef.src);
+  nextImageIndex = (nextImageIndex + galleryItems.length + direction) % galleryItems.length;
   lightboxImageRef.src = galleryItems[nextImageIndex].original;
   lightboxImageRef.alt = galleryItems[nextImageIndex].description;
 };
 
 // Event Listeners
-galleryListRef.addEventListener('click', event => {
+galleryListRef.addEventListener('click', (event) => {
   event.preventDefault();
   if (event.target.classList.contains('gallery__image')) {
     lightboxImageRef.setAttribute('src', event.target.dataset.source);
@@ -60,7 +55,7 @@ galleryListRef.addEventListener('click', event => {
   }
 });
 
-lightboxRef.addEventListener('click', event => {
+lightboxRef.addEventListener('click', (event) => {
   if (
     event.target.dataset.action === 'close-lightbox' ||
     event.target.classList.contains('lightbox__overlay')
@@ -105,5 +100,7 @@ if (currentEvent) {
   const body = document.querySelector('body');
   const happy = document.createElement('h2');
   happy.textContent = currentEvent;
+  happy.style.marginTop = '200px';
+  happy.style.textAlign = 'center';
   body.appendChild(happy);
 }

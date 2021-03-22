fd1c02f34c7549b9d1f43d62b5804473c1028cf7
Dmitriy Nikolaiev <dimnik.u11@gmail.com>
2021-03-22 10:40:27 +0200

    Добавил настройки загрузчиков и плагинов webpack

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import galleryItems from './gallery-items';
+import './css/common.scss';
+
+console.log('Hello Webpack!');
+console.log('Now is on webpack dev server)');
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,4 +8,31 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          // 'style-loader',
+          //
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'src/index.html' }),
+    new MiniCssExtractPlugin({ filename: 'styles.css' }),
+  ],
+  devServer: { compress: true, port: 8080 },
 };
